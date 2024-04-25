0c07d0cb414d76df916780892ef0033a2291e300
an1604 <nataf12386@gmail.com>
2024-04-25 16:30:58 +0300

    first commit

--- client.py
+++ client.py
@@ -1,80 +1,116 @@
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
 import socket
-from datetime import datetime, timedelta
+import traceback
+import time
+import errno
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+from Crypto.Protocol.KDF import scrypt
+from Crypto.Util.Padding import pad, unpad
 
-from server import PORT, HOST
+SERVER_HOST = '127.0.0.1'
+SERVER_PORT = 2030
+
+
+def generate_symmetric_key(password, salt, key_size=16):
+    key = scrypt(password, salt, key_size, N=2 ** 14, r=8, p=1)
+    return key
+
+
+def encrypt_message(message, key):
+    cipher = AES.new(key, AES.MODE_CBC)
+    ciphertext = cipher.encrypt(pad(message, AES.block_size))
+    return ciphertext, cipher.iv
+
+
+def decrypt_message(ciphertext, key, iv):
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    decrypted_message = unpad(cipher.decrypt(ciphertext), AES.block_size)
+    return decrypted_message
 
 
 class Client:
-    def __init__(self, client_name, client_port, server_ip, server_port):
+    def __init__(self, client_name, client_port, file_name=None):
         self.client_name = client_name
         self.client_ip = None
         self.client_port = client_port
-        self.server_ip = server_ip
-        self.server_port = server_port
-        self.server_PK = None  # The server's public key, to encrypt the messages while sending to the server.
-        self.first_reply = True  # Flag to check if the server's public key is coming.
-        self.deadline_time = 10  # The deadline time to store all the messages that sent from the client in seconds.
 
-        # Prefix building
-        ip = '_'.join(self.server_ip.split('.'))
-        port = str(self.server_port)
-        self.prefix = f'{ip} {port}'
+        self.server_PK = None  # The server's public key, to encrypt the messages while sending to the server.
+        self.server_PK_cipher = None
+        self.symmetric_key = None  # The symmetric key for the communication between Alice and Bob.
+
+        # Extracting the file's parameters for the communication.
+        if file_name:
+            with open(file_name, 'rb') as f:
+                data = f.read()
+                params = data.split(' ')
+                self.m = params[0]
+                self.servers_path = params[1].split(',')
+                self.sending_round = params[2]
+                self.password = params[3]
+                self.salt_password = params[4]
+                self.dest_ip = params[5]
+                self.dest_port = params[6]
+                self.symmetric_key = generate_symmetric_key(password=self.password, salt=self.salt_password)
+        else:
+            self.dest_ip = '_'.join(SERVER_HOST.split('.'))
+            self.dest_port = str(SERVER_PORT)
+
+        # Prefix building for responding.
+        self.prefix = f'{self.dest_ip} {self.dest_port}'
 
         self.messages_sent = []
         self.messages_received = []
 
-        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # The connection socket
         self.run_client()
 
     def run_client(self):
-        try:
-            self.client.connect((self.server_ip, self.server_port))
-            self.client_ip = self.client.getsockname()[0]
-            PK = self.client.recv(1024).decode()  # Get the public key of the server.
-            self.server_PK = PK
+        self.client.connect((SERVER_HOST, SERVER_PORT))
+        PK = self.client.recv(1024).decode()  # Get the public key of the server.
+        self.server_PK = RSA.import_key(PK)
+        self.server_PK_cipher = PKCS1_OAEP.new(self.server_PK)
 
-            receiver_name = input("Enter receiver name: ")
+        print(f"Server's public key is yours, {PK}")
+        user_input = ''
 
-            start_time = datetime.now()
-            while True:
-                user_input = input(f'Message to {receiver_name}:')
-                if user_input.lower() == 'exit':
-                    break
+        while not 'exit' in user_input.lower():
+            user_input = input('Message:')
+            self.messages_sent.append(user_input)
+            message_to_server = self.prefix + ' ' + user_input  # Concatenate the prefix to the actual message
+            message_to_server_encoded = message_to_server.encode()
 
-                self.messages_sent.append({
-                    'to': receiver_name,
-                    'msg': user_input
-                })
+            # The encryption part uses the public key of the server
+            encrypted_message = self.server_PK_cipher.encrypt(message_to_server_encoded)
+            self.client.send(encrypted_message)
 
-                # Checks the timer (10 sec timer)
-                current_time = datetime.now()
-                if current_time - start_time >= timedelta(seconds=self.deadline_time):
-                    print('timeout!!!')
-                    print("Sending the messages...")
+            self.receive_data()
 
-                    # Sent all the messaged one by one to the server.
-                    for message in self.messages_sent:
-                        msg = self.prefix + ' ' + message['msg']  # Chain the prefix with the actual data.
-                        message_encoded = msg.encode()
-                        print(f"Message encoded from type: {type(message_encoded)}")
+        self.client.send(user_input.lower().encode())  # Sent the exit command to the server
+        self.client.close()
 
-                        print(f'Sending message: {message}, the encode : {message_encoded}')
-                        self.client.send(message_encoded)
+    def receive_data(self, timeout=2.5):
+        self.client.setblocking(False)
+        start_time = time.time()
+        while True:
+            try:
+                data_received = self.client.recv(1024).decode()
+                if data_received:
+                    print(f"Response: {data_received}")
+            except socket.error as e:
+                # Handle non-blocking socket exception
+                if e.errno == errno.EWOULDBLOCK:
+                    pass  # No data available yet
+                else:
+                    print("Error:", e)
+                    traceback.print_exc()
 
-                    data = self.client.recv(1024).decode()
-                    if data:
-                        self.messages_received.append({
-                            'from': receiver_name,
-                            'msg': data,
-                        })
-                        print(f'{receiver_name}: {data}')
-                    start_time = datetime.now()  # Restart the timer.
-
-            self.client.close()
-
-        except Exception as e:
-            print(f"Error: {e}")
+            # Check timeout
+            if time.time() - start_time >= timeout:
+                break
+        self.client.setblocking(True)  # Restore blocking mode
 
 
 if __name__ == '__main__':
-    client = Client(client_name='alice', client_port=1234, server_port=PORT, server_ip=HOST)
+    c = Client(client_name='alice', client_port=1234)
--- server.py
+++ server.py
@@ -1,30 +1,71 @@
 from Crypto.PublicKey import RSA
 import socket
-import random
-from datetime import datetime
-from clientHandler import ClientHandler
+from datetime import datetime, timedelta
+from Crypto.Cipher import PKCS1_OAEP
+import traceback
 
+import concurrent.futures
 
-def generate_rsa_keys(key_size=2048):  # The RSA key generation for the server
-    key = RSA.generate(key_size)
-    p_key = key.publickey().export_key()
-    s_key = key.export_key()
-    return p_key, s_key
-
-
-# TCP Host & Port
 HOST = '127.0.0.1'
 PORT = 2030
 
 
+def generate_rsa_keys(key_size=2048, PK_file_name=None, SK_file_name=None):  # The RSA key generation for the server
+    if SK_file_name is None and PK_file_name is None:
+        # If the filename is None, we generate a 2048-bit random key.
+        key = RSA.generate(key_size)
+        p_key = key.publickey().export_key()
+        s_key = key.export_key()
+    else:  # Otherwise, we're reading the key from the file
+        with open(SK_file_name, 'r') as file:
+            s_key = RSA.import_key(file.read()).export_key()
+        with open(PK_file_name, 'rb') as file:
+            p_key = RSA.import_key(file.read()).export_key()
+
+    return p_key, s_key
+
+
+def handle_client(client_socket: socket, client_address, PK, message_len, SK, deadline_time):
+    try:
+        client_socket.send(PK)  # Send the server's public key to the client
+        msgs = []  # List of all messages
+        start_time = datetime.now()
+
+        SK = RSA.import_key(SK)  # Activating the secret key of the server.
+        cipher = PKCS1_OAEP.new(SK)
+
+        data = ''
+        while not 'exit' in str(data).lower():
+            data = client_socket.recv(1024)
+            decrypted_data = cipher.decrypt(data).decode()
+            data = decrypted_data.encode()
+            msgs.append(data)
+
+            # Timeout occurrence.
+            current_time = datetime.now()
+            if current_time - start_time >= timedelta(seconds=deadline_time):
+                for m in msgs:  # Iterate over all the messages, and sent them back to the client.
+                    client_socket.send(m)
+
+                msgs.clear()  # Clear the messages' list till the next timeout
+                start_time = datetime.now()  # Restart the timer.
+    except Exception as e:
+        print(f"Error: {e}")
+        traceback.print_exc()
+    finally:
+        client_socket.close()  # Close the client socket when done
+
+
 class Server:
     def __init__(self):
-        self.stop = False
         self.PK, self.SK = generate_rsa_keys()  # The keys' generation phase.
         self.sock = socket.socket(socket.AF_INET,
                                   socket.SOCK_STREAM)  # The main socket that will bind to the HOST & PORT
         self.message_len = 1000  # 1000 bytes for a single message.
+        self.deadline_time = 10  # The deadline time for the server keeps client's messages before sent them.
         self.clients = []  # List of clients with their names, ports,and ip addresses.
+        self.pool = concurrent.futures.ThreadPoolExecutor(max_workers=5)  # Thread-pool executor
+
         print(f"The server is listening on {HOST}:{PORT}")
         self.sock.bind((HOST, PORT))  # The actual bind.
         self.sock.listen()
@@ -33,12 +74,10 @@ class Server:
 
     def run_server(self):
         while True:
-            client_sock, client_add = self.sock.accept()  # Accepting new connections (blocking call)
-            print("Client hits the server!")
-
-            # Setting a new thread for every client that hits the server.
-            client_handler = ClientHandler(client_sock, self.PK, self.SK, self.message_len)
-            client_handler.run()
+            client_socket, client_address = self.sock.accept()
+            print(f"New client connected: {client_address}")
+            self.pool.submit(handle_client, client_socket, client_address, self.PK, self.message_len, self.SK,
+                             self.deadline_time)
 
 
 if __name__ == '__main__':
